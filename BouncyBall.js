f07131e77845791645de12137563209b36de86a4
Reicher <robin.o.reicher@gmail.com>
2014-01-18 14:35:56 +0100

    Added a line, hehe

--- BouncyBall.js
+++ BouncyBall.js
@@ -1,6 +1,22 @@
 var c=document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
 
+function Wall () {
+	this.p1 =  new Array();
+	this.p2 =  new Array();
+	
+	this.p1 = [60, 50];
+	this.p2 = [0, 50];
+	
+	this.Draw = function () {
+		ctx.beginPath();
+		ctx.moveTo(700, 50);
+		ctx.lineTo(700, 500);
+		ctx.closePath();
+		ctx.stroke();
+	};
+}
+
 function Ball () {
 	this.x = 400;
 	this.y = 300;
@@ -11,13 +27,13 @@ function Ball () {
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, 50, 0, Math.PI*2, true);  // Cool
 		ctx.closePath();
-		ctx.fill();
+		ctx.stroke();
 	};
     	
 	this.Update = function () {
 		this.x += this.vx * dt;
 		this.y += this.vy * dt;
-	}
+	};
 }
 
 function Update(dt) {
@@ -35,11 +51,16 @@ function Draw(){
 	ctx.rect(0,0, c.width, c.height);
 	ctx.stroke();
 
+	// Wall
+	aWall.Draw();
+
 	// Ball
 	myBall.Draw();
+	
 }
 
 var myBall = new Ball();
+var aWall = new Wall();
 
 // Start the game loop
 fps = 30;
