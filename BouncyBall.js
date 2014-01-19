05bc23b09ff38f6943b83d3681cf17a11b6cfe14
Reicher <robin.o.reicher@gmail.com>
2014-01-19 21:34:32 +0100

    Added a moveable wall, works pretty good but treats the ball like a point

--- BouncyBall.js
+++ BouncyBall.js
@@ -1,5 +1,7 @@
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
+/* By Robin Reicher!
+May or may not work in firefox and older ie broswers
+*/
+
 
 var dotLineLength = function(x, y, x0, y0, x1, y1, o) {
   function lineLength(x, y, x0, y0){
@@ -26,29 +28,65 @@ function dotproduct(a,b) {
 	return n;
 };
 
-function Wall (p1x, p1y, p2x, p2y) {
-	this.p1 =  new Array();
-	this.p2 =  new Array();
+function rotate_point( around, angle, point)
+{
+	s_thing = Math.sin(angle);
+	c_thing = Math.cos(angle);
 
-	this.p1x = p1x;
-	this.p1y = p1y;
-	this.p2x = p2x;
-	this.p2y = p2y;	
+	// translate point back to origin:
+	point[0] -= around[0];
+	point[1] -= around[1];
+
+	// rotate point
+	xnew = point[0] * c_thing + point[1] * s_thing;
+	ynew = point[0] * s_thing - point[1] * c_thing;
+
+	// translate point back:
+	point[0] = xnew + around[0];
+	point[1] = ynew + around[1];
+
+	return point;
+};
+
+function Wall (p1, p2) {
+	this.p1 =  p1;
+	this.p2 =  p2;
+
+	this.rotation = 0;
 	
 	this.Draw = function () {
 		ctx.beginPath();
-		ctx.moveTo(this.p1x, this.p1y);
-		ctx.lineTo(this.p2x, this.p2y);
+		ctx.moveTo(this.p1[0], this.p1[1]);
+		ctx.lineTo(this.p2[0], this.p2[1]);
 		ctx.closePath();
 		ctx.stroke();
+	};
+
+	this.rotate = function(){
+		var mid = [ (this.p1[0] + this.p2[0]) / 2, (this.p1[1] + this.p2[1]) / 2 ];
+		this.p1 = [ mid[0] - 50, mid[1] ];
+		this.p2 = [ mid[0] + 50, mid[1] ];
+		this.p1 = rotate_point(mid, this.rotation, this.p1);
+		this.p2 = rotate_point(mid, this.rotation, this.p2);
+	};
+
+	this.moveTo = function(pos){
+		this.p1 = [ pos[0] - 50, pos[1] ];
+		this.p2 = [ pos[0] + 50, pos[1] ];
+
+		this.rotate();
+	};
+
+	this.isLeft = function(p){
+     		return ((p2[0] - p1[0]) * (p[1] - p1[1]) - (p2[1] - p1[1]) * (p[0] - p1[0])) > 0;
 	};
 }
 
 function Ball () {
 	this.pos = new Array(400, 200);
-	this.vel = new Array(70, 50);
+	this.vel = new Array(100, -70);
 	this.size = 50;
-	this.bouncy = 0.75;
+	this.bouncy = 0.8;
 	
 	this.Draw = function () {
 		ctx.beginPath();
@@ -65,40 +103,41 @@ function Ball () {
 	
 	this.Gravity = function ()
 	{
-		gravConst = 110;
+		gravConst = 160;
 		this.vel[1] += gravConst * dt;
 	};
 	
 	this.CheckForCollision = function (wall) {
-		if( dotLineLength(this.pos[0], this.pos[1], wall.p1x, wall.p1y, wall.p2x, wall.p2y, true) < this.size + 1){
-			dx = wall.p2x - wall.p1x;
-			dy = wall.p2y - wall.p1y;
+		dist = dotLineLength(this.pos[0], this.pos[1], wall.p1[0], wall.p1[1], wall.p2[0], wall.p2[1], true) 
+			- this.size;
+		if(dist <= 0 ){
+			dx = wall.p2[0] - wall.p1[0];
+			dy = wall.p2[1] - wall.p1[1];
 			
-			var normal1 = new Array(dy, -dx);
-			var normal2 = new Array(-dy, dx);
+			var normal = wall.isLeft(this.pos) ? new Array(-dy, dx) : new Array(dy, -dx);
 			
-			// Normalize normals:)
-			length = Math.sqrt( Math.pow(normal1[0], 2) + Math.pow(normal1[1], 2));
-			normal1[0] =  normal1[0]/length;
-			normal1[1] =  normal1[1]/length;
+			// Normalize normal:)
+			length = Math.sqrt( Math.pow(normal[0], 2) + Math.pow(normal[1], 2));
+			normal[0] =  normal[0]/length;
+			normal[1] =  normal[1]/length;
 
-			length = Math.sqrt( Math.pow(normal2[0], 2) + Math.pow(normal2[1], 2));
-			normal2[0] =  normal2[0]/length;
-			normal2[1] =  normal2[1]/length;
+			// Correct position
+			this.pos[0] += normal[0] * Math.abs(dist);
+			this.pos[1] += normal[1] * Math.abs(dist);
 
-			var tmp = dotproduct( normal2, this.vel );
-			this.vel[0] = this.vel[0] - 2 * tmp * normal2[0]; // * this.bouncy;
-			this.vel[1] = this.vel[1] - 2 * tmp * normal2[1]; // * this.bouncy;
+			var tmp = dotproduct( normal, this.vel );
+			this.vel[0] = this.vel[0] - 2 * tmp * normal[0] * this.bouncy;
+			this.vel[1] = this.vel[1] - 2 * tmp * normal[1] * this.bouncy;
 
 		}
 	};
 }
 
 function Update() {
+	myBall.CheckForCollision(myWall);
 	for (var i = 0; i < walls.length; i++)
 		myBall.CheckForCollision(walls[i]);
 	
-
 	myBall.Update(dt);
 };
 
@@ -114,19 +153,35 @@ function Draw(){
 	ctx.stroke();
 
 	// Wall
+	myWall.Draw();
 	for (var i = 0; i < walls.length; i++)
 		walls[i].Draw();
 	
-
 	// Ball
 	myBall.Draw();
-	
 }
 
+function updateMousePos(event)
+{
+	myWall.moveTo([event.x, event.y -50]);
+}
+
+function updateMouseWheel(event)
+{
+	perTick = Math.PI * 2 / 360 * (event.wheelDelta / 20);
+	myWall.rotation += perTick;
+
+	myWall.rotate();
+}
+
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+
 var myBall = new Ball();
-var walls = [	new Wall(50, 500, 700, 500), 
-		new Wall(700, 0, 650, 600), 
-		new Wall(100, 100, 300, 600)];
+var myWall = new Wall( [350, 550], [450, 550] );
+var walls = [	new Wall( [50, 500],  [700, 550] ), 
+		new Wall( [700, 0],   [650, 600] ), 
+		new Wall( [100, 100], [100, 600] )];
 
 // Start the game loop
 fps = 30;
@@ -135,6 +190,10 @@ dt = 1 / interval;
 
 setInterval(Update, interval);
 setInterval(Draw, interval);
+
+c.addEventListener("mousemove", updateMousePos, true);
+c.addEventListener("mousewheel", updateMouseWheel, true);
+
 
 // To stop the game, use the following:
 //clearInterval(Game._intervalId);
