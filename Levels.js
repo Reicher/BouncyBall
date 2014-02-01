fc821d973996fb77cbe49d068ac30471a5c26538
Reicher <robin.o.reicher@gmail.com>
2014-02-01 20:18:10 +0100

    fucked lvl1 up-bugfix

--- Levels.js
+++ Levels.js
@@ -72,7 +72,7 @@ function updateLevel(level){
 ////////////////////////// LEVELS ///////////////////////////////////////////
 function Level1() {
 	this.walls = [	new Wall( [20, 20],  [580, 20] ), 
-			new Wall( [20, 20],   [0, 600] ), 
+			new Wall( [20, 20],   [0, 800] ), 
 			new Wall( [580, 20], [600, 800] )];
 			
 	this.ballSize = 20;
